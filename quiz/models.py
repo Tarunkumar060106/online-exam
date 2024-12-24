0031fe03011d6fa5b1ca0805f1ad58486e904114
S Tarunkumar <ts0147@srmist.edu.in>
2024-12-24 14:12:26 +0530

    update models.py

--- quiz/models.py
+++ quiz/models.py
@@ -15,7 +15,7 @@ class Subject(models.Model):
         return self.subject_name
     
 class Section(models.Model):
-    subject_id = models.ForeignKey(Subject, on_delete=models.CASCADE)
+    subject = models.ForeignKey(Subject, on_delete=models.CASCADE)
     section_name = models.CharField(max_length=50)
     def __str__(self):
         return f"{self.section_name} ({self.subject_id.subject_name})"
