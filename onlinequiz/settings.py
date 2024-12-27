4a1f5ec1a6da6356e3ed21956f3c27de212b93ec
S Tarunkumar <ts0147@srmist.edu.in>
2024-12-27 22:16:10 +0530

    update settings.py

--- onlinequiz/settings.py
+++ onlinequiz/settings.py
@@ -92,16 +92,16 @@ WSGI_APPLICATION = 'onlinequiz.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-        # 'NAME': 'vizhuchelvamdb',
-        # 'USER': 'root',
-        # 'PASSWORD': 'pass123',
-        # 'HOST': '127.0.0.1',
-        # 'PORT': '3306',
-        # 'OPTIONS': {
-        #     'charset': 'utf8mb4',
-        # },
+        'ENGINE': 'django.db.backends.mysql',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': 'vizhuchelvamdb',
+        'USER': 'root',
+        'PASSWORD': 'pass123',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+        'OPTIONS': {
+            'charset': 'utf8mb4',
+        },
     }
 }
 
