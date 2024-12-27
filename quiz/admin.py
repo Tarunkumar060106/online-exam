cfd285175b81ebde1a15c34c06df54689bbd657c
S Tarunkumar <ts0147@srmist.edu.in>
2024-12-28 01:16:39 +0530

    update -> admin.py

--- quiz/admin.py
+++ quiz/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+from models import Course, Subject, Section, Difficulty, Objectives, Question, Result
+admin.site.register(Course)
+admin.site.register(Subject)
+admin.site.register(Section)
+admin.site.register(Difficulty)
+admin.site.register(Objectives)
+admin.site.register(Question)
+admin.site.register(Result)
